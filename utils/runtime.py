9b6ea3bced80205af7e3e2f6a5d74dfa07979038
Stanislav Lapshin <sm.lapshin@gmail.com>
2022-02-04 15:43:43 +0300

    refactor: auto detect cuda/cpu

--- utils/runtime.py
+++ utils/runtime.py
@@ -1,5 +1,7 @@
 import torch
 
-device_code = "gpu" if torch.cuda.is_available() else "cpu"
+device_code = "gpu" if torch.cuda.is_available() else "cuda"
 
 device = torch.device(device_code)
+
+print("detected device: {0}".format(device_code))
